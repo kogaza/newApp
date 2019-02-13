20cf3419fb7468986ec3fccfabf2b837f71988ba
Krzysztof Ogaza <krzychu_ten@o2.pl>
2019-02-13 14:01:43 +0100

    MapStateToProps

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,42 +1,42 @@
 // import { routerMiddleware } from 'connected-react-router/immutable';
-import { History } from 'history';
-// import { createLogger } from 'redux-logger';
+// import { History } from 'history';
+import {createLogger} from 'redux-logger';
 // import createSagaMiddleware from 'redux-saga';
 // import thunkMiddleware from 'redux-thunk';
 import rootReducers from '../reducers/index';
 // import appSaga from 'sagas/index';
 
-import { createBrowserHistory } from 'history';
-import { applyMiddleware, compose, createStore } from 'redux';
-import { routerMiddleware } from 'connected-react-router';
+import {createBrowserHistory} from 'history';
+import {applyMiddleware, compose, createStore} from 'redux';
+import {routerMiddleware} from 'connected-react-router';
 
 // export default function configureStore(history: History) {
-    // const composeEnhancer: typeof compose = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// const composeEnhancer: typeof compose = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-    // const sagaMiddleware = createSagaMiddleware();
+// const sagaMiddleware = createSagaMiddleware();
 
-    // const middleware = [
-    //     thunkMiddleware,
-    //     routerMiddleware(history),
-    //     sagaMiddleware,
-    // ];
+// const middleware = [
+//     thunkMiddleware,
+//     routerMiddleware(history),
+//     sagaMiddleware,
+// ];
 
-    // if (process.env.NODE_ENV !== 'production') {
-    //     middleware.push(createLogger({
-    //         // ...options
-    //     }));
-    // }
+// if (process.env.NODE_ENV !== 'production') {
+//     middleware.push(createLogger({
+//         // ...options
+//     }));
+// }
 
-    // const store = createStore(
-    //     rootReducers(history),
-        // composeEnhancer(
-        //     applyMiddleware(...middleware)
-        // )
-    // );
+// const store = createStore(
+//     rootReducers(history),
+// composeEnhancer(
+//     applyMiddleware(...middleware)
+// )
+// );
 
-    // sagaMiddleware.run(appSaga);
+// sagaMiddleware.run(appSaga);
 
-    // return store;
+// return store;
 // }
 
 
@@ -45,16 +45,19 @@ import { routerMiddleware } from 'connected-react-router';
 export const history = createBrowserHistory()
 
 export default function configureStore(preloadedState: any) {
-    const store = createStore(
-        rootReducers(history), // root reducer with router state
-        preloadedState,
-        compose(
-            applyMiddleware(
-                routerMiddleware(history), // for dispatching history actions
-                // ... other middlewares ...
-            ),
-        ),
-    )
-    console.warn(store);
-    return store
+  const store = createStore(
+    rootReducers(history), // root reducer with router state
+    preloadedState,
+    compose(
+      applyMiddleware(
+        routerMiddleware(history), // for dispatching history actions
+        createLogger({
+          // ...options
+        })
+        // ... other middlewares ...
+      ),
+    ),
+  )
+
+  return store
 }
